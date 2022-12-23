cf760565b41a3062a8340f9c758bd985043c9c56
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-12-23 10:49:25 +0300

    Trying to add a notification socket

---
+++ controllers/notification.js
@@ -0,0 +1,32 @@
+// Notification controller
+// Path: controllers\notification.js
+const {
+  createNotification,
+  getNotification,
+} = require("../modules/notification");
+//
+const notificationHandler = (socket) => {
+  // Join notification  room
+  socket.on("join_notification_room", async (data) => {
+    console.log("Joining notification room", data);
+    socket.join(data);
+  });
+
+  //  send a new notification
+  socket.on("send_notification", async (data) => {
+    const notification = await createNotification(data);
+    console.log("Notification :", notification)
+    
+    socket.to(data.recieverId).emit("new_notification", notification);
+  });
+
+  // get all notifications
+  socket.on("get_notification", async (data) => {
+    const notification = await getNotification(data);
+    socket.emit("all_notification", notification);
+  });
+};
+
+module.exports = {
+  notificationHandler,
+};
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 const { chatMessageHandler } = require("./controllers/chat");
 const { teamMessageHandler } = require("./controllers/team");
+const { notificationHandler } = require("./controllers/notification");
 
 // IO instance
 const io = new Server(PORT, {
@@ -22,6 +23,8 @@ const io = new Server(PORT, {
 // NameSpaces
 const chatNamespace = io.of("/chat");
 const teamNameSpace = io.of("/team");
+// Notification dinamic namespace
+const notificationNameSpace = io.of("/notification");
 
 chatNamespace.on("connection", async (socket) => {
   chatMessageHandler(socket);
@@ -31,8 +34,11 @@ teamNameSpace.on("connection", async (socket) => {
   teamMessageHandler(socket);
 });
 
+notificationNameSpace.on("connection", async (socket) => {
+  notificationHandler(socket);
+});
+
 instrument(io, {
   auth: false,
   mode: "development",
 });
-
