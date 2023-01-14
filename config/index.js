573c9d14d6c61b297a891678cf683fa8b306f1ce
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-01-14 17:15:15 +0300

    Removed the functionality ofn sending creating messages from RTCServer and moved it to the Main server

--- config/index.js
+++ config/index.js
@@ -4,9 +4,18 @@ const PORT = process.env.PORT || 5000;
 const USER_URI = process.env.MONGODB_URI;
 const SECRETE = process.env.SECRETE;
 
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const BUCKET_REGION = process.env.BUCKET_REGION;
+const BUCKET_ACCESS_KEY = process.env.BUCKET_ACCESS_KEY;
+const BUCKET_SECRETE_KEY = process.env.BUCKET_SECRETE_KEY;
+
 console.log(`SECRETE: ${SECRETE}`);
 module.exports = {
   PORT,
   USER_URI,
   SECRETE,
+  BUCKET_NAME,
+  BUCKET_REGION,
+  BUCKET_ACCESS_KEY,
+  BUCKET_SECRETE_KEY,
 };
--- controllers/chat.js
+++ controllers/chat.js
@@ -1,5 +1,4 @@
-const User = require("../model/user");
-const myModule = require("../modules/messege");
+
 
 const chatMessageHandler = (socket) => {
   socket.on("join_room", async (data) => {
@@ -7,23 +6,9 @@ const chatMessageHandler = (socket) => {
   });
 
   socket.on("send_message", async (data) => {
-    const user = await User.findById(data.userId);
-
-    const chatRoomId = data.id;
-    const messegeSent = data.newMessage.message;
-    const idFromClient = data.newMessage.idFromClient;
-    // Saving messege
-    const messege = await myModule.addingMessegeInChatRoom(
-      user,
-      chatRoomId,
-      messegeSent,
-      idFromClient
-    );
-    console.log("Messege :", messege);
-    // // Return the messege to the client
-    socket.emit("messege_sent", messege);
-
-    socket.to(chatRoomId).emit("receive_message", messege);
+    console.log("Messege :", data);
+    console.log("chatRoom :", data.chatRoom);
+    socket.to(data.chatRoom).emit("receive_message", data);
   });
 };
 
